c572ca2c260ada8617e78357a5d88b571c9a980e
orkun <kulceorkunfun@gmail.com>
2017-02-15 22:06:44 +0100

    deploy.sh added
    some changes has been made on application.conf

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-if test ! -z $1
+if test  -z $1
 then
     echo "parameter couldn't found"
     exit -1
 fi
 OUTPUT=`activator playGenerateSecret | grep "Generated new secret: " | sed 's/.*: // '`
+OUTPUT="'${OUTPUT}'"
 echo $OUTPUT
+activator dist
+boxfuse run -env=$1 -jvm.args=-DAPPLICATION_SECRET=$OUTPUT
 exit 0
