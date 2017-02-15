787e8535af383c08f84193926363d50d33eb2ac2
orkun <kulceorkunfun@gmail.com>
2017-02-15 20:36:15 +0100

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 IMSProject2.0
+
+I wanted to improve IMSProject with better code styling and performance.
+Still developing the project.
--- app/services/database/DatabaseController.scala
+++ app/services/database/DatabaseController.scala
@@ -2,10 +2,13 @@ package services.database
 
 import akka.actor.ActorSystem
 import com.google.inject.Inject
+import models.Room
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
+import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 /**
   * Created by orkun on 10/02/17.
@@ -13,5 +16,5 @@ import scala.concurrent.Future
 class DatabaseController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, val system: ActorSystem) extends  HasDatabaseConfigProvider[JdbcProfile]
                                                                                                                     with DatabaseTrait with SlickDatabaseMapping {
 
-  //def insertRoom(password: Seq[String]) : Future[Unit] = db.run(RoomTable += )
+  def insertRoom(room: Room) : Future[Unit] = db.run(RoomTable += room).map(_ => ())
 }
